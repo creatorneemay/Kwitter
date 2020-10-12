418b536d2bb639aac71bac5caa9b83b2dce52299
creatorneemay <72016905+creatorneemay@users.noreply.github.com>
2020-10-12 18:56:20 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -12,11 +12,34 @@ var firebaseConfig = {
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
+    username = localStorage.getItem("username");
+    document.getElementById("user_name").innerHTML="Welcome " + username;
+    function add_room(){
+          roomname=document.getElementById("room_name").value;
+          localStorage.setItem("roomname",roomname);
+          firebase.database().ref("/").child(roomname).update({
+                purpose : "adding roomname"
+          });
+          window.location="kwitterpage.html";
+    }
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+       console.log("roomname: "+ Room_names);
+       row="<div class='room_name' id="+Room_names+" onclick='redirecttoroomname(this.id)'>#"+Room_names+ "</div> <hr>";
+       document.getElementById("output").innerHTML+=row;
       //End code
       });});}
 getData();
+ 
+function redirecttoroomname(name){
+      console.log(name);
+      localStorage.setItem("roomname",name);
+      window.location="kwitterpage.html";
+}
+function logout(){
+      localStorage.removeItem("username");
+      localStorage.removeItem("roomname");
+      window.location= "index.html";
+}
